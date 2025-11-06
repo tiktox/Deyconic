65cfe44e5d4117f677d9f41048f44978a8e0d146
tiktox <sp9979182@gmail.com>
2025-11-06 08:16:54 -1200

    12

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,3 +1,28 @@
+import { ImageResponse } from 'next/og';
+
+export const runtime = 'edge';
+
 export default function Icon() {
-  return null;
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 24,
+          background: 'black',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+        }}
+      >
+        D
+      </div>
+    ),
+    {
+      width: 32,
+      height: 32,
+    }
+  );
 }
